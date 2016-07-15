cbca1d3eb2cf31b9931fb3a4b66ba8620540cb7f
star <hope0510@naver.com>
2016-07-15 16:29:32 +0900

    ui manager 추가

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.UI;
+public class Main : MonoBehaviour
+{
+	//스크립트 생성시 최초 1번 호출
+	void Awake()
+	{
+		UIManager.Instance.Initialize();
+	}
+	//스크립트 소멸 직전 호출
+	void OnDestroy()
+	{
+		UIManager.Instance.Terminate();
+	}
+	void Start()
+	{
+		UIManager.Instance.ActivateUILayer(UILayerType.LOBBY);
+	}
+	void Update()
+	{
+		UIManager.Instance.UpdateCustom();
+	}
+}
---
+++ Assets/Scripts/UI/UILayerType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.UI
+{
+	public enum UILayerType
+	{
+		GAME,
+		GAME_LOADING,
+		LOBBY,
+		MAX,
+		NONE = MAX,
+	}
+}
